462526004f0b67e0f5ba547cea9dbea629dcf570
Ole Troan <otroan@employees.org>
2022-04-02 11:39:23 +0200

    Initial commit.
    Signed-off-by: Ole Troan <ot@cisco.com>

---
+++ fmv.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+from ast import AugAssign
+import datetime
+from typing import IO
+import urllib3
+from urllib3 import request
+import certifi
+import json
+from datetime import date, datetime, timedelta
+from typing import Union, Tuple
+import numpy as np
+
+class FMV():
+    def __init__(self, fetch_stock_data=False) -> None:
+        self.symbols = {}
+        self.fetch_stock_data = fetch_stock_data
+
+    def fetch_stock(self, symbol):
+        '''Returns a dictionary of date and closing value'''
+        apikey='LN6PYRQ0I5LKDY51'
+        http = urllib3.PoolManager()
+        # The REST api is described here: https://www.alphavantage.co/documentation/
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&' \
+                'apikey={apikey}'
+        r = http.request('GET', url)
+        if r.status != 200:
+            raise Exception(f'Fetching stock data for {symbol} failed {r.status}')
+        raw = json.loads(r.data.decode('utf-8'))
+        return {k: float(v['4. close'])
+               for k, v in raw['Time Series (Daily)'].items()}
+
+    def fetch_currency(self, currency):
+        '''Returns a dictionary of date and closing value'''
+        http = urllib3.PoolManager()
+        # The REST api is described here: https://app.norges-bank.no/query/index.html#/no/
+        # url = f'https://data.norges-bank.no/api/data/EXR/B.{currency}.NOK.SP?startPeriod=2000&format=sdmx-json'
+        url = f'https://data.norges-bank.no/api/data/EXR/B.{currency}.NOK.SP?startPeriod=2000&format=csv-:-comma-false-y'
+        r = http.request('GET', url)
+        if r.status != 200:
+            raise Exception(f'Fetching currency data for {currency} failed {r.status}')
+        cur = {}
+        for i, line in enumerate(r.data.decode('utf-8').split('\n')):
+            if i == 0 or ',' not in line: continue  # Skip header and blank lines
+            d, exr = line.strip().split(',')
+            c = float(exr.strip('"'))
+            d = d.strip('"')
+            cur[d] = c
+        return cur
+
+    def get_filename(self, symbol):
+        return f'data/{symbol}.json'
+
+    def load(self, symbol):
+        filename = self.get_filename(symbol)
+        with open(filename, 'r') as f:
+            self.symbols[symbol] = json.load(f)
+
+    def need_refresh(self, symbol, d: datetime.date):
+        if symbol not in self.symbols:
+            return True
+        fetched = self.symbols[symbol]['fetched']
+        fetched = datetime.strptime(fetched, '%Y-%m-%d').date()
+        if d > fetched:
+            return True
+        return False
+
+    def refresh(self, symbol, d: datetime.date, currency):
+        if not self.need_refresh(symbol, d):
+            return
+
+        filename = self.get_filename(symbol)
+
+        # Try loading from cache
+        try:
+            with open(filename, 'r') as f:
+                self.symbols[symbol] = json.load(f)
+                if not self.need_refresh(symbol, d):
+                    return
+        except IOError:
+            pass
+
+        if currency:
+            data = self.fetch_currency(symbol)
+            print('DATA', data)
+        else:
+            data = self.fetch_stock(symbol)
+
+        print(f'Writing to {filename}')
+        data['fetched'] = str(date.today())
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+        self.symbols[symbol] = data
+
+    def parse_date(self, date: Union[str, datetime]) -> Tuple[datetime.date, str]:
+        '''Parse date/timestamp'''
+        if type(date) is str:
+            date = datetime.strptime(date, '%Y-%m-%d').date()
+        else:
+            date = date.date()
+        date_str = str(date)
+        return date, date_str
+
+
+    def __getitem__(self, item):
+        symbol, date = item
+        date, date_str = self.parse_date(date)
+        self.refresh(symbol, date, False)
+        for i in range(5):
+            try:
+                return self.symbols[symbol][date_str]
+            except KeyError:
+                # Might be a holiday, iterate backwards
+                date -= timedelta(days=1)
+                date_str = str(date)
+        return np.nan
+
+    def get_currency(self, currency, date: Union[str, datetime]):
+        date, date_str = self.parse_date(date)
+        self.refresh(currency, date, True)
+        for i in range(5):
+            try:
+                return self.symbols[currency][date_str]
+            except KeyError:
+                # Might be a holiday, iterate backwards
+                date -= timedelta(days=1)
+                date_str = str(date)
+        return np.nan
+
+
+if __name__ == '__main__':
+
+    f = FMV()
+    print('LOOKING UP DATA', f['CSCO', '2021-12-31'])
+
+    #print('LOOKING UP DATA', f['CSCO', '2022-12-31'])
+
+    print('LOOKING UP DATA', f['SLT', '2021-12-31'])
+
+    #f.fetch_currency('USD')
+    print('LOOKING UP DATA USD2NOK', f.get_currency('USD', '2021-12-31'))
---
+++ trans.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import json
+import os
+from fmv import FMV
+
+class TDTransactionsJSON():
+    def __init__(self, json_file, prev_holdings, year) -> None:
+        with open(json_file, 'r') as jf:
+                json_data = json.load(jf)
+        self.df = pd.json_normalize(json_data)
+        self.df_trades = None
+        self.df_dividend = None
+        self.df_tax = None
+        self.year = year
+
+        # Get holdings from previous year
+        dfprevh = None
+        if os.path.isfile(prev_holdings):
+            dfprevh = pd.read_json(prev_holdings)
+            dfprevh['date'] = pd.to_datetime(dfprevh['date'])
+        else:
+            print('**** NO PREVIOUS HOLDINGS IS THIS REALLY FIRST YEAR? ****')
+        self.dfprevh = dfprevh
+
+    def trades(self):
+        # Create trades dataframe
+        if self.df_trades is not None:
+            return self.df_trades
+
+
+        ### TODO: Move these to __init__
+        tmp = self.df[(self.df.type.isin(['TRADE', 'RECEIVE_AND_DELIVER'])) & (self.df['transactionItem.instrument.assetType'] == 'EQUITY')]
+        if len(tmp) == 0:
+            self.df_trades = self.dfprevh
+            return self.dfprevh
+        df = pd.DataFrame()
+        df[['type', 'symbol', 'date',
+                'qty', 'price', 'amount']] = tmp[['transactionItem.instruction',
+                                                                            'transactionItem.instrument.symbol',
+                                                                            'transactionDate',
+                                                                            'transactionItem.amount',
+                                                                            'transactionItem.price',
+                                                                            'netAmount']]
+        df['qty'] = np.where(df['type'] == 'SELL', -1 * df['qty'], df['qty'])
+
+        f = FMV()
+
+        # Filter out transactions for given year
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+        df = df.sort_values(by='date')
+        df['price_nok'] = df.apply(lambda row: (row['price'] * f.get_currency('USD', row['date'])), axis=1)
+        df['tax_deduction'] = 0
+
+        if self.dfprevh is not None:
+            df = pd.concat([self.dfprevh, df])
+
+        df['idx'] = pd.Index(range(len(df.index)))
+
+        self.df_trades = df
+        return df
+
+    def dividends(self):
+        # Create dividends dataframe
+        if self.df_dividend is not None:
+            return self.df_dividend
+
+        tmp = self.df[(self.df.type == 'DIVIDEND_OR_INTEREST') & (self.df['transactionItem.instrument.assetType'] == 'EQUITY')].sort_values(by='transactionDate')
+        df = pd.DataFrame()
+        df[['symbol', 'date', 'dividend']] = tmp[['transactionItem.instrument.symbol',
+                                                  'transactionDate',
+                                                  'netAmount']]
+
+        # Filter out transactions for given year
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+
+        self.df_dividend = df
+        return df
+
+    def tax(self):
+        # Create taxs dataframe
+        if self.df_tax is not None:
+            return self.df_tax
+
+        tmp = self.df[(self.df.type == 'JOURNAL') & (self.df.description == 'W-8 WITHHOLDING')].sort_values(by='transactionDate')
+        df = pd.DataFrame()
+        df[['symbol', 'date', 'tax']] = tmp[['transactionItem.instrument.symbol',
+                                             'transactionDate',
+                                             'netAmount']]
+
+        # Filter out transactions for given year
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+
+        self.df_tax = df
+        return df
+
+def description_to_type(value):
+    print('VALUE', value, type(value))
+    if value.startswith('Bought') or value.startswith('TRANSFER OF SECURITY'):
+        return 'BUY'
+    if value.startswith('Sold'):
+        return 'SELL'
+    if value.startswith('ORDINARY DIVIDEND'):
+        return 'DIVIDEND'
+    if value.startswith('W-8 WITHHOLDING'):
+        return 'TAX'
+    if value.startswith('CLIENT REQUESTED ELECTRONIC FUNDING DISBURSEMENT'):
+        return 'WIRE'
+    return value
+
+
+class TDTransactionsCSV():
+    def __init__(self, csv_file, prev_holdings, year) -> None:
+        df = pd.read_csv(csv_file, converters={'DESCRIPTION': description_to_type})
+        self.df_trades = None
+        self.df_dividend = None
+        self.df_tax = None
+        self.year = year
+
+        df = df.rename(columns={'DATE': 'date',
+                                'TRANSACTION ID': 'transaction_id',
+                                'DESCRIPTION': 'type',
+                                'SYMBOL': 'symbol',
+                                'QUANTITY': 'qty',
+                                'PRICE': 'price',
+                                'FUND REDEMPTION FEE': 'fee1',
+                                'SHORT-TERM RDM FEE': 'rdm',
+                                ' DEFERRED SALES CHARGE': 'fee2',
+                                'COMMISSION': 'fee3',
+                                'AMOUNT': 'amount',
+                                })
+
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+        df = df.sort_values(by='date')
+        df['qty'] = np.where(df['type'] == 'SELL', -1 * df['qty'], df['qty'])
+
+        # Get holdings from previous year
+        dfprevh = None
+        if os.path.isfile(prev_holdings):
+            dfprevh = pd.read_json(prev_holdings)
+            dfprevh['date'] = pd.to_datetime(dfprevh['date'])
+        else:
+            print('**** NO PREVIOUS HOLDINGS IS THIS REALLY FIRST YEAR? ****')
+        self.dfprevh = dfprevh
+        self.df = df
+
+    def trades(self):
+        # Create trades dataframe
+        if self.df_trades is not None:
+            return self.df_trades
+
+
+        df = self.df[self.df.type.isin(['BUY', 'SELL'])]
+        df = pd.concat([self.dfprevh, df])
+
+        self.df_trades = df
+        return self.df_trades
+
+    def dividends(self):
+        # Create dividends dataframe
+        if self.df_dividend is not None:
+            return self.df_dividend
+
+        tmp = self.df[self.df.type == 'DIVIDEND_OR_INTEREST'].sort_values(by='transactionDate')
+        df = pd.DataFrame()
+        df[['symbol', 'date', 'dividend']] = tmp[['transactionItem.instrument.symbol',
+                                                  'transactionDate',
+                                                  'netAmount']]
+
+        # Filter out transactions for given year
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+
+        self.df_dividend = df
+        return df
+
+    def tax(self):
+        # Create taxs dataframe
+        if self.df_tax is not None:
+            return self.df_tax
+
+        tmp = self.df[(self.df.type == 'JOURNAL') & (self.df.description == 'W-8 WITHHOLDING')].sort_values(by='transactionDate')
+        df = pd.DataFrame()
+        df[['symbol', 'date', 'tax']] = tmp[['transactionItem.instrument.symbol',
+                                             'transactionDate',
+                                             'netAmount']]
+
+        # Filter out transactions for given year
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['date'].dt.year == self.year]
+
+        self.df_tax = df
+        return df
+
+
+# year=2021
+# csv_file = f'data/tdameritrade-{year}.csv'
+# prev_holdings = f'data/holdings-{str(year - 1)}.json'   
+# td = TDTransactionsCSV(csv_file, prev_holdings, year)  
+# df = td.trades()
+
