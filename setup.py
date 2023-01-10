2ba9112dc8f84e90506f329965d6794b2d73715b
Ole Troan <otroan@employees.org>
2023-01-10 08:36:29 +0100

    fix setup.py
    Signed-off-by: Ole Troan <otroan@cisco.com>

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="espp2",  # Required
-    version="0.0.1",  # Required
-    description="A tax tool for ESPP/RSU amd foreign held shares",  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type="text/markdown",  # Optional (see note above)
-    url="https://github.com/pypa/sampleproject",  # Optional
-    author="A. Random Developer",  # Optional
-    author_email="author@example.com",  # Optional
-    packages=find_packages(),  # Required
+    name="espp2",
+    version="0.0.1",
+    description="A tax tool for ESPP/RSU amd foreign held shares",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/otroan/ESPP2",
+    author="O. Troan",
+    author_email="otroan@employees.org",
+    packages=find_packages(),
     python_requires=">=3.9, <4",
-    install_requires=["simplejson"],  # Optional
-    package_data={  # Optional
+    install_requires=["simplejson", "numpy"],
+    package_data={
         "espp2": ["*.json"],
     },
-    entry_points={  # Optional
+    entry_points={
         "console_scripts": [
             ['espp2=espp2.espp2:main',
             'espp2_transnorm=espp2.transnorm:main'],
