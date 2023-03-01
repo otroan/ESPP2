828f6ed0d9a16279fa821fadd6aa8c4d945e05a8
Ole Troan <otroan@cisco.com>
2023-03-01 11:06:47 +0100

    test_web
    Signed-off-by: Ole Troan <otroan@cisco.com>

---
+++ espp2/web/test_web.py
@@ -0,0 +1,10 @@
+# Description: Test the web application
+
+from fastapi.testclient import TestClient
+from espp2.web.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
