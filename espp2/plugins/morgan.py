f9a37ec5777f1ba2bd0ee286557088b85281426d
Morten Rolland <mortenro@acm.org>
2023-03-16 07:03:39 +0100

    morgan-stanley import: Work in progress checkpoint commit
    This is an intermediary commit that seems to make the ledger
    look ok, but work is remaining regarding dividends handling
    and taxes withholding.

--- espp2/plugins/morgan.py
+++ espp2/plugins/morgan.py
@@ -13,13 +13,157 @@ from pandas import read_html
 from pydantic import parse_obj_as
 from espp2.fmv import FMV
 from espp2.datamodels import Transactions, Entry, EntryTypeEnum, Amount
+import simplejson as json
+import re
 
 currency_converter = FMV()
+
+class ParseState:
+    def __init__(self, filename):
+        self.source = f'morgan:{filename}'
+        self.transactions = []
+        self.activity = '<unknown>'
+        self.symbol = None
+        self.entry_date = None
+
+    def preparse_row(self, row):
+        '''Parse common aspects of a row, return True for further parsing'''
+        # Set entry-date, or symbol for special header-lines
+        date = getitem(row, 'Entry Date')
+        if date is None:
+            raise ValueError(f'Entry-date is not provided for {row}')
+
+        # Check for indication of which stock is to follow
+        m = re.match(r'''^Fund:\s+([A-Za-z]+)\s''', date)
+        if m:
+            self.symbol = m.group(1)
+            if self.symbol == 'Cash':
+                self.symbol = None
+            return False    # No more parsing needed
+
+        self.entry_date = dt.parse(date)
+        self.activity = getitem(row, 'Activity')
+
+        return True
+
+    def deposit(self, qty, purchase_price, description, purchase_date=None):
+        assert self.symbol is not None
+
+        r = { 'type': EntryTypeEnum.DEPOSIT,
+              'date': self.entry_date,
+              'qty': qty,
+              'symbol': self.symbol,
+              'description': description,
+              'purchase_price': purchase_price,
+              'purchase_date': purchase_date,
+              'source': self.source,
+              'broker': 'morgan' }
+
+        self.transactions.append(parse_obj_as(Entry, r))
+
+    def sell(self, qty, price):
+        assert self.symbol is not None
+
+        r = { 'type': EntryTypeEnum.SELL,
+              'date': self.entry_date,
+              'qty': qty,
+              'amount': fixup_price(self.entry_date, 'USD', f'{price}'),
+              'symbol': self.symbol,
+              'description': self.activity,
+              'source': self.source }
+
+        self.transactions.append(parse_obj_as(Entry, r))
+
+    def parse_rsu_release(self, row):
+        '''Handle what appears to be RSUs added to account'''
+        m = re.match(r'''^Release\s+\(([A-Z0-9]+)\)''', self.activity)
+        if not m:
+            return False
+
+        id = m.group(1)     # Unused for now
+        qty, value, ok = getitems(row, 'Number of Shares', 'Book Value')
+        if not ok:
+            raise ValueError(f'Missing columns for {row}')
+        qty = Decimal(qty)
+        book_value, currency = morgan_price(value)
+        purchase_price = fixup_price2(self.entry_date, currency, book_value / qty)
+
+        self.deposit(qty, purchase_price, 'RS', self.entry_date)
+        return True
+
+    def parse_dividend_reinvest(self, row):
+        '''Reinvestment of dividend through bying same share'''
+        if self.activity != 'You bought (dividend)':
+            return False
+
+        qty, price, ok = getitems(row, 'Number of Shares', 'Share Price')
+        if not ok:
+            raise ValueError(f'Missing columns for {row}')
+        qty = Decimal(qty)
+        price, currency = morgan_price(price)
+        purchase_price = fixup_price2(self.entry_date, currency, price)
+
+        self.deposit(qty, purchase_price, 'Dividend re-invest')
+        return True
+
+    def parse_sale(self, row):
+        if self.activity != 'Sale':
+            return False
+        qty, price, ok = getitems(row, 'Number of Shares', 'Share Price')
+        if not ok:
+            raise ValueError(f'Missing colummns for {row}')
+        price, currency = morgan_price(price)
+        qty = Decimal(qty)
+        price = Decimal(price)
+
+        self.sell(qty, price)
+        return True
+
+    def parse_deposit(self, row):
+        if self.activity != 'Share Deposit' and self.activity != 'Historical Purchase':
+            return False
+        qty, ok = getitems(row, 'Number of Shares')
+        if not ok:
+            raise ValueError(f'Missing columns for {row}')
+        qty = Decimal(qty)
+        price = currency_converter[(self.symbol, self.entry_date)]
+        purchase_price = fixup_price2(self.entry_date, 'USD', price)
+
+        self.deposit(qty, purchase_price, 'ESPP', self.entry_date)
+        return True
+
+    def parse_dividend_cash(self, row):
+        '''This, despite its logged description, results in shares-reinvest'''
+        if self.activity != 'Dividend (Cash)':
+            return False
+        qty, ok = getitems(row, 'Number of Shares')
+        if ok:
+            qty = Decimal(qty)
+            price = currency_converter[(self.symbol, self.entry_date)]
+            purchase_price = fixup_price2(self.entry_date, 'USD', price)
+            self.deposit(qty, purchase_price, 'Dividend re-invest (Cash)', self.entry_date)
+
+        return True
+
+    def parse_tax_withholding(self, row):
+
+        r = { 'type': EntryTypeEnum.TAX,
+              'date': self.entry_date,
+              'amount': amount,
+              'symbol': self.symbol,
+              'description': self.activity }
+
+        # ___TODO!
+
 def morgan_price(price_str):
     '''Parse price string.'''
     # import IPython
     # IPython.embed()
-    value, currency = price_str.split(' ')
+    if ' ' in price_str:
+        value, currency = price_str.split(' ')
+    else:
+        value, currency = price_str, 'USD'
+
     return Decimal(value.replace('$', '').replace(',', '')), currency
 
 def fixup_price(datestr, currency, pricestr, change_sign=False):
@@ -39,9 +183,107 @@ def fixup_price2(date, currency, value):
                    nok_exchange_rate=exchange_rate,
                    nok_value=value * exchange_rate)
 
-def table(recs, filename):
+def getitem(row, colname):
+    '''Get a named item from a row, or None if nothing there'''
+    if colname not in row:
+        return None
+    item = row[colname]
+    if isinstance(item, float) and math.isnan(item):
+        return None
+    assert isinstance(item, str)
+    if item == '':
+        return None
+    return item
+
+def getitems(row, *colnames):
+    ok = True
+    rc = []
+    for cn in colnames:
+        i = getitem(row, cn)
+        rc.append(i)
+        if i is None:
+            ok = False
+    rc.append(ok)
+    return tuple(rc)
+
+def dumpdict(dct):
+    print(json.dumps(dct, use_decimal=True, indent=4))
+
+def get_entry_date(row):
+    item = getitem(row, 'Entry Date')
+    if item is None:
+        raise ValueError(f'Expected entry-date for row {row}')
+    return dt.parse(item)
+
+def parse_rsu_table(state, recs, filename):
+    ignore = {
+        'Opening Value': True,
+        'Closing Value': True,
+        'Dividend (Cash)': True,
+        'IRS Nonresident Alien Withholding': True,
+    }
+
+    for row in recs:
+        if not state.preparse_row(row):
+            continue
+
+        if state.parse_rsu_release(row):
+            continue
+
+        if state.parse_dividend_reinvest(row):
+            continue
+
+        if state.parse_sale(row):
+            continue
+
+        if state.activity in ignore:
+            continue
+
+        raise ValueError(f'Unknown activity: "{state.activity}"')
+
+def parse_espp_table(state, recs, filename):
+    ignore = {
+        'Opening Value': True,
+        'Closing Value': True,
+        'IRS Nonresident Alien Withholding': True,
+        'Adhoc Adjustment': True,
+        'Transfer out': True,
+        'Withholding': True,
+        'Historical Transaction': True,
+        'Wash Sale Adjustment': True,
+    }
+
+    for row in recs:
+        if not state.preparse_row(row):
+            continue
+
+        if state.parse_dividend_reinvest(row):
+            continue
+
+        if state.parse_sale(row):
+            continue
+
+        if state.parse_deposit(row):
+            continue
+
+        if state.parse_dividend_cash(row):
+            continue
+
+        if state.activity in ignore:
+            continue
+
+        raise ValueError(f'Unknown activity2: "{state.activity}"')
+
+    return state.transactions
+
+def old_parse_table(recs, filename):
     '''Parse Morgan Stanley HTML table transaction history.'''
     trans = []
+
+    print('---- Other:')
+    dumpdict(recs)
+    print('--')
+
     for e in recs:
         # print('RECORD:', e)
         # Header row
@@ -80,9 +322,13 @@ def table(recs, filename):
             r = {'type': t, 'date': d, 'amount': amount, 'symbol': symbol, 'description': e['Activity']}
 
         elif e['Activity'] == 'Dividend (Cash)':
+            #
+            print(f'>>> Dividend (Cash): {e}')
+            tmpcash = e['Cash']
+            if isinstance(tmpcash, float) and math.isnan(tmpcash):
+                continue
             t = EntryTypeEnum.DIVIDEND
-            amount = fixup_price(d, "USD", e['Cash'])
-
+            amount = fixup_price(d, "USD", tmpcash)
             r = {'type': t, 'date': d, 'amount': amount, 'symbol': symbol}
 
         elif e['Activity'] == 'You bought (dividend)':
@@ -107,10 +353,11 @@ def table(recs, filename):
 
             t = EntryTypeEnum.SELL
             qty = Decimal(e['Number of Shares'])
-            if not math.isnan(e['Cash']):
-                amount = fixup_price(d, "USD", e['Cash'])
-            else:
+            tmpcash = e['Cash']
+            if isinstance(tmpcash, float) and math.isnan(tmpcash):
                 amount = fixup_price(d, "USD", e['Book Value'])
+            else:
+                amount = fixup_price(d, "USD", e['Cash'])
 
             r = {'type': t, 'date': d, 'qty': qty, 'amount': amount, 'symbol': symbol, 'description': e['Activity']}
 
@@ -123,6 +370,10 @@ def table(recs, filename):
             # Opening Value of cash in shares account
             continue
 
+        elif e['Activity'] == 'Historical Purchase':
+            # Probably ESPP but we get the data from somewhere else
+            continue
+
         else:
             raise ValueError(f'Unknown activity type: {e["Activity"]}: {e}')
 
@@ -133,16 +384,18 @@ def table(recs, filename):
 def morgan_html_import(html_fd, filename):
     '''Parse Morgan Stanley HTML table file.'''
 
+    state = ParseState(filename)
 
     # Extract the cash and stocks activity tables
     dfs = read_html(
         html_fd, header=1, attrs={'class': 'sw-datatable', 'id': 'Activity_table'})
 
-    trans = []
-    for df in dfs:
-        trans += table(df.to_dict(orient='records'), filename)
+    assert(len(dfs) == 2)
 
-    return Transactions(transactions=trans)
+    parse_rsu_table(state, dfs[0].to_dict(orient='records'), filename)
+    parse_espp_table(state, dfs[1].to_dict(orient='records'), filename)
+
+    return Transactions(transactions=sorted(state.transactions, key=lambda d: d.date))
 
 def read(html_file, filename='', logger=None) -> Transactions:
     '''Main entry point of plugin. Return normalized Python data structure.'''
