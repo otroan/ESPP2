a97ac2a5afef8b1c8b6eb65b824e02b57ffb96f1
Ole Troan <otroan@cisco.com>
2023-04-12 11:37:51 +0200

    ensure generate holdings always return a holdings object
    Signed-off-by: Ole Troan <otroan@cisco.com>

--- espp2/web/main.py
+++ espp2/web/main.py
@@ -27,27 +27,22 @@ async def generate_holdings_1(
         transaction_files: list[UploadFile],
         broker: str = Form(...),
         holdfile: UploadFile | None = None,
-        opening_balance: str = Form(...),
+        # opening_balance: str = Form(...),
         year: int = Form(...)):
     '''Generate previous year holdings from a plethora of transaction files'''
     opening_balance = None
 
-    if opening_balance:
-        opening_balance = json.loads(opening_balance)
-        opening_balance = parse_obj_as(Holdings, opening_balance)
+    # if opening_balance:
+    #     opening_balance = json.loads(opening_balance)
+    #     opening_balance = parse_obj_as(Holdings, opening_balance)
 
     if holdfile and holdfile.filename == '':
         holdfile = None
     elif holdfile:
         holdfile = holdfile.file
     try:
-        holdings = do_holdings_1(
+        return do_holdings_1(
             broker, transaction_files, holdfile, year, opening_balance=opening_balance)
-        if type(holdings) == list:
-            p = Positions(year - 1, holdings, [], received_wires=Wires(__root__=[]))
-            p.process()
-            holdings = p.holdings(year - 1, broker)
-        return holdings
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=500, detail=str(e)) from e
@@ -63,12 +58,7 @@ async def generate_holdings_2(
         expected_balance = json.loads(expected_balance)
         expected_balance = parse_obj_as(ExpectedBalance, expected_balance)
     try:
-        holdings = do_holdings_2(broker, transaction_files, year, expected_balance=expected_balance)
-        if type(holdings) == list:
-            p = Positions(year - 1, holdings, [], received_wires=Wires(__root__=[]))
-            p.process()
-            holdings = p.holdings(year - 1, broker)
-        return holdings
+        return do_holdings_2(broker, transaction_files, year, expected_balance=expected_balance)
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=500, detail=str(e)) from e
