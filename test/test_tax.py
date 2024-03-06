c8f9041a418674099877dd18716a7ae53bcdc46f
Ole Troan <otroan@employees.org>
2024-03-06 09:28:32 +0100

    fix unittests
    Signed-off-by: Ole Troan <otroan@cisco.com>

--- test/test_tax.py
+++ test/test_tax.py
@@ -3,7 +3,7 @@ from importlib.resources import files
 from rich.console import Console
 from rich.table import Table
 
-from espp2.datamodels import Transactions, Dividend, EntryTypeEnum, Amount, Deposit, Wire
+from espp2.datamodels import Transactions, Dividend, EntryTypeEnum, Amount, PositiveAmount, Deposit, Wire
 from espp2.positions import Positions, Cash
 from espp2.main import tax_report
 import json
@@ -17,17 +17,17 @@ def test_dividends(caplog):
     transactions = []
 
     transactions.append(Deposit(type=EntryTypeEnum.DEPOSIT, date='2022-08-26', symbol='CSCO',
-            qty=100, purchase_date='2022-10-26', 
+            qty=100, purchase_date='2022-10-26',
             purchase_price=Amount(currency='USD', value=10, nok_value=100, nok_exchange_rate=10),
             description='', source='test'))
 
     transactions.append(Deposit(type=EntryTypeEnum.DEPOSIT, date='2022-10-10', symbol='CSCO',
-            qty=100, purchase_date='2022-10-26', 
+            qty=100, purchase_date='2022-10-26',
             purchase_price=Amount(currency='USD', value=10, nok_value=100, nok_exchange_rate=10),
             description='', source='test'))
 
     d = Dividend(type=EntryTypeEnum.DIVIDEND, date='2022-10-26', symbol='CSCO',
-                 amount=Amount(currency='USD', value=38, nok_value=100, nok_exchange_rate=10),
+                 amount=PositiveAmount(currency='USD', value=38, nok_value=100, nok_exchange_rate=10),
                  source='test')
     assert(d.exdate == datetime.date(2022, 10, 4))
     transactions.append(d)
@@ -46,13 +46,13 @@ def test_dividends(caplog):
 def test_wire():
     #  type=<EntryTypeEnum.WIRE: 'WIRE'> date=datetime.date(2022, 11, 25)
     #  amount=Amount(currency='USD', nok_exchange_rate=Decimal('9.9263'),
-    #                 nok_value=Decimal('-243548'), value=Decimal('-24535.66')) 
-    #  description='Cash Disbursement' fee=Amount(currency='USD', 
+    #                 nok_value=Decimal('-243548'), value=Decimal('-24535.66'))
+    #  description='Cash Disbursement' fee=Amount(currency='USD',
     #                                             nok_exchange_rate=Decimal('9.9263'), nok_value=Decimal('-148.894'), value=Decimal('-15.00')) source='schwab:../Stocks/data/EquityAwardsCenter_Transactions_20221222153044.csv' id='WIRE 2022-11-25:1'
 
     w = Wire(type=EntryTypeEnum.WIRE, date='2022-11-25',
              amount=Amount(currency='USD', value=-24535.66, nok_value=-243548, nok_exchange_rate=9.9263),
              source='test',
              description='Cash Disbursement',)
-    
+
     assert w.fee == None
