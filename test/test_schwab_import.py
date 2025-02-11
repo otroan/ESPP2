3355879e7cafef5de9cde5dbc89d4ccab27e3fc8
Ole Troan <otroan@cisco.com>
2025-02-11 14:02:37 +0100

    Remove Schwab CSV plugin
    Signed-off-by: Ole Troan <otroan@cisco.com>

--- test/test_schwab_import.py
+++
@@ -1,9 +0,0 @@
-
-import espp2.plugins.schwab as schwab
-from espp2.datamodels import Transactions
-
-
-def test_schwab_import():
-    with open("test/schwab.csv", "r", encoding="utf-8") as f:
-        t = schwab.read(f)
-    assert isinstance(t, Transactions)
