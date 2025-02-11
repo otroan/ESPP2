1c2c71a7a510db21c6fba9d5753ad76f0207d40a
Ole Troan <otroan@employees.org>
2025-02-11 14:00:58 +0100

    Remove pickle support
    Signed-off-by: Ole Troan <otroan@cisco.com>

--- test/test_pickle_import.py
+++
@@ -1,9 +0,0 @@
-
-import espp2.plugins.pickle as pickle
-from espp2.datamodels import Transactions
-
-
-def test_pickle_import():
-    with open("test/espp.pickle", "rb") as f:
-        t = pickle.read(f)
-    assert isinstance(t, Transactions)
